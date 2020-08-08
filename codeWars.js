b5af428b52f5c4ecff36bd45722c906e7622576e
Drews8 <50556437+Drews8@users.noreply.github.com>
2020-08-08 14:46:54 +0300

    Update codeWars.js

--- codeWars.js
+++ codeWars.js
@@ -1,1 +1,21 @@
+//--------------------------------------------------------------------------------------------------------------------------------------------Simple string reversal---------------
+//Description:
+//In this Kata, we are going to reverse a string while maintaining the spaces (if any) in their original place.
+//
+//For example:
+//
+//solve("our code") = "edo cruo"
+//-- Normal reversal without spaces is "edocruo". 
+//-- However, there is a space after the first three characters, hence "edo cruo"
+//
+//solve("your code rocks") = "skco redo cruoy"
+//solve("codewars") = "srawedoc"
 
+function solve(str) {
+   let result = str.replace(/ /g, '').split('').reverse()
+   for (let i = 0; i < str.length; i++) {
+     str[i] === ' ' ? result.splice(i, 0, ' ') : null
+   }
+   return result.join('')
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
